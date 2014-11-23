e9954f8463e54be6adeefcf7ea38b583023db18e
Danny Wan <dannywan@mac.com>
2014-11-24 01:24:26 +0800

    2 commit

--- plot6.R
+++ plot6.R
@@ -18,5 +18,5 @@ NEI1<-aggregate(Emissions~year+fips, NEI1, FUN=sum)
 
 #Create the plot
 png(file="plot6.png", width = 480, height = 480, units = "px")
-ggplot(NEI1, aes(x=year, y=Emissions, group=fips, color=fips)) +geom_line() +labs(title="Comparison of Motor Vehicle Emissions - Baltimore City vs Los Angeles", x="Year", y="Total Emissions")
+ggplot(NEI1, aes(x=year, y=Emissions, group=fips, color=fips)) +geom_line() +labs(title="Comparison of Motor Vehicle Emissions - Baltimore City vs LA", x="Year", y="Total Emissions")
 dev.off()
